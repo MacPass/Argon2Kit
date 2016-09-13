0aa0bafaaa11b32cd82b8bee81531408c95d16a2
michael starke <michael.starke@hicknhack-software.com>
2016-09-13 15:06:28 +0200

    created wrapper framework

--- Argon2Kit/Argon2Kit.h
+++ Argon2Kit/Argon2Kit.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char Argon2KitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Argon2Kit/PublicHeader.h>
 
-
+#include "argon2.h"
