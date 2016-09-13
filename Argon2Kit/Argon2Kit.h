6d1e4bcfc4bb97d9d3c2b287f07455f71494924a
michael starke <michael.starke@hicknhack-software.com>
2016-09-13 14:56:02 +0200

    Initial Commit

---
+++ Argon2Kit/Argon2Kit.h
@@ -0,0 +1,19 @@
+//
+//  Argon2Kit.h
+//  Argon2Kit
+//
+//  Created by Michael Starke on 13/09/16.
+//  Copyright © 2016 HicknHack Software GmbH. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Argon2Kit.
+FOUNDATION_EXPORT double Argon2KitVersionNumber;
+
+//! Project version string for Argon2Kit.
+FOUNDATION_EXPORT const unsigned char Argon2KitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Argon2Kit/PublicHeader.h>
+
+
